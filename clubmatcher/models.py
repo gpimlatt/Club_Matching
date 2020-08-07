2889d5d7b4b178839c3007fe38c14096ce9658c6
Nikolas North <nikolasn09@gmail.com>
2020-08-07 14:41:29 -0400

    Create many-to-many relationship for clubs and tags

--- clubmatcher/models.py
+++ clubmatcher/models.py
@@ -8,11 +8,19 @@ def load_user(user_id):
     return Club.query.get(int(user_id))
 
 
+club_tags = db.Table(
+    'club_tags',
+    db.Column('club_id', db.Integer, db.ForeignKey('club.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
 class Club(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(60), nullable=False),
+    description = db.Column(db.Text),
     ecommerce = db.Column(db.Text, nullable=False, default='https://www.westernusc.store/club-memberships/')
     facebook = db.Column(db.Text)
     instagram = db.Column(db.Text)
@@ -35,3 +43,12 @@ class Club(db.Model, UserMixin):
 
     def __repr__(self):
         return f"Club(name='{self.name}', answers=[{self.answers}])"
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    clubs = db.relationship('Club', secondary=club_tags, backref='tags', lazy=True)
+
+    def __repr__(self):
+        return f"Tag(name={self.name})"
