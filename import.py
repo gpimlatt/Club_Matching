1b25973e52e40260a8111d371ed9ba24d46ba84b
Nikolas North <nikolasn09@gmail.com>
2020-08-10 12:38:54 -0400

    Remove unused category variables

--- import.py
+++ import.py
@@ -6,12 +6,6 @@ from clubmatcher.models import Club, Tag
 password = os.environ.get('USC_CLUB_MATCHER_PASSWORD')
 hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-community_tag = Tag.query.get(1)
-philanthropy_tag = Tag.query.get(2)
-hobbies_tag = Tag.query.get(3)
-health_tag = Tag.query.get(4)
-cultural_tag = Tag.query.get(5)
-
 
 def add_tag_to_db(name):
     if not Tag.query.filter_by(name=name).first():
