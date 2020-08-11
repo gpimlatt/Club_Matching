a3247c126b75148c21cc2cd52d882597fdf6f8a9
Nikolas North <nikolasn09@gmail.com>
2020-08-11 10:32:46 -0400

    Add SERVER_NAME app config to allow for sending emails

--- clubmatcher/__init__.py
+++ clubmatcher/__init__.py
@@ -21,7 +21,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
-    app.config['SERVER_NAME'] = '127.0.0.1:5000'
+    app.config['SERVER_NAME'] = 'localhost:5000'
 
     db.init_app(app)
     bcrypt.init_app(app)
