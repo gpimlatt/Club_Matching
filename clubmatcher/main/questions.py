abd521f1633b1d2155eb3d43e81238ae92afa680
Nikolas North <nikolasn09@gmail.com>
2020-09-08 13:04:00 -0400

    Remove club_results html template and edit questions file

--- clubmatcher/main/questions.py
+++ clubmatcher/main/questions.py
@@ -3,6 +3,12 @@ q1_a1 = 'Just generally being social with other like-minded students.'
 q1_a2 = 'I want to meet students of similar backgrounds.'
 q1_a3 = 'I want to give back to the community.'
 q1_a4 = "I'm looking to enhance my educational experience."
+q1_answers = {
+    1: "Just generally being social with other like-minded students.",
+    2: "I want to meet students of similar backgrounds.",
+    3: "I want to give back to the community.",
+    4: "I'm looking to enhance my educational experience."
+}
 
 q2 = 'What overall category of club would you be interested in?'
 q2_a1 = 'Academics'
@@ -14,31 +20,67 @@ q2_a6 = 'Hobbies / Special Interests'
 q2_a7 = 'Music / Performing Arts'
 q2_a8 = 'Politics/Social Justice'
 q2_a9 = 'Sports'
+q2_answers = {
+    1: "Academics",
+    2: "Business/Finance",
+    3: "Community Volunteering / Philanthropy",
+    4: "Cultural",
+    5: "Health and Wellness",
+    6: "Hobbies / Special Interests",
+    7: "Music / Performing Arts",
+    8: "Politics/Social Justice",
+    9: "Sports"
+}
 
 q3 = 'Are you looking to join a larger or smaller organization?'
 q3_a1 = 'I would rather be a part of a smaller, more intimate group.'
 q3_a2 = "More than a couple dozen and I can't remember any names!"
 q3_a3 = 'The bigger, the better.'
 # q3_a4 = "I don't care; size doesn't matter."
+q3_answers = {
+    1: "I would rather be a part of a smaller, more intimate group.",
+    2: "More than a couple dozen and I can't remember any names!",
+    3: "The bigger, the better."
+}
 
 q4 = 'What level of commitment are you prepared for?'
 q4_a1 = 'I can only see myself being available several times in a semester.'
 q4_a2 = 'A few times a month would probably be my upper limit.'
 q4_a3 = 'Once or more a week would be awesome!'
 # q4_a4 = 'Time is not a factor for me.'
+q4_answers = {
+    1: "I can only see myself being available several times in a semester.",
+    2: "A few times a month would probably be my upper limit.",
+    3: "Once or more a week would be awesome!"
+}
 
 q5 = 'Let’s talk money. Would you be willing to spend money throughout the year to attend special events (ie, bar nights, speakers, or trips) with your new friends?'
 q5_a1 = 'I really hope to avoid spending additional money throughout the year.'
 q5_a2 = 'Maybe the rare occasion.'
 q5_a3 = 'Of course I would. You only live once, right?'
 # q5_a4 = "I would like some details first, but I'm not particularly opposed."
+q5_answers = {
+    1: "I really hope to avoid spending additional money throughout the year.",
+    2: "Maybe the rare occasion.",
+    3: "Of course I would. You only live once, right?"
+}
 
 q6 = 'How important for you is a group with a strong focus on advocacy - either for its members or for another marginalised community?'
 q6_a1 = 'Totally not interested.'
 q6_a2 = 'Would be just as happy to talk politics for the fun of it.'
 q6_a3 = 'I am so down for discussion and debate.'
+q6_answers = {
+    1: "Totally not interested.",
+    2: "Would be just as happy to talk politics for the fun of it.",
+    3: "I am so down for discussion and debate."
+}
 
 q7 = 'What about a dress code?'
 q7_a1 = "I don't want to go anywhere I can't wear my O-Week T-shirt and sweatpants."
 q7_a2 = "I'm malleable. I'll dress up if I have to."
 q7_a3 = "Any opportunity to get dressed up and I'll take it!"
+q7_answers = {
+    1: "I don't want to go anywhere I can't wear my O-Week T-shirt and sweatpants.",
+    2: "I'm malleable. I'll dress up if I have to.",
+    3: "Any opportunity to get dressed up and I'll take it!"
+}
