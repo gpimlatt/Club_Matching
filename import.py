ec3a0620c3350eaa95a9737d9f6b4e5bf915d617
Nikolas North <nikolasn09@gmail.com>
2020-09-18 15:39:28 -0400

    Modify import script

--- import.py
+++ import.py
@@ -5,30 +5,6 @@ from clubmatcher import create_app, db, bcrypt
 from clubmatcher.main.models import Club, Tag
 
 
-def add_tag_to_db(name):
-    if not Tag.query.filter_by(name=name).first():
-        db.session.add(Tag(name=name))
-
-
-def import_tags(filepath):
-    with open(filepath) as file:
-        tags = json.load(file)
-    for tag in tags:
-        add_tag_to_db(tag['Name'])
-    db.session.commit()
-
-
-def add_ecommerce_url_and_description(filepath):
-    with open(filepath) as file:
-        clubs = json.load(file)
-    for club_data in clubs:
-        club = Club.query.get(club_data['SKU'])
-        if club:
-            club.ecommerce = club_data['StoreURL']
-            club.description = club_data['Short description']
-            db.session.commit()
-
-
 def import_clubs(filepath):
     with open('etc/config.json') as file:
         config = json.load(file)
@@ -36,13 +12,21 @@ def import_clubs(filepath):
     with open(filepath) as file:
         clubs = json.load(file)
     for club in clubs:
-        if club['Organization Email']:
+        if club['Email']:
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            tag = Tag.query.filter_by(name=club['Tags']).first()
+            if not tag:
+                tag = Tag(name=club['Tags'])
+                db.session.add(tag)
             new_club = Club(
-                id=club['Organization ID'],
-                name=club['Organization Name'],
-                email=club['Organization Email'],
-                password=hashed_password
+                id=club['SKU'],
+                name=club['Name'],
+                email=club['Email'],
+                password=hashed_password,
+                description=club['Short description'],
+                ecommerce=club['Storefront Link'],
+                western_link=club['WL Address'],
+                tag=tag
             )
             db.session.add(new_club)
     db.session.commit()
@@ -51,6 +35,4 @@ def import_clubs(filepath):
 app = create_app()
 if __name__ == '__main__':
     with app.app_context():
-        # import_tags('data/tags.json')
-        # import_clubs('data/clubs-2.json')
-        add_ecommerce_url_and_description('data/club_ecommerce.json')
+        import_clubs('data/clubs.json')
