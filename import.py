509a5bbea30569058990da82b174735b5f4d87ca
Nikolas North <nikolasn09@gmail.com>
2020-09-20 19:12:53 -0400

    Update club data

--- import.py
+++ import.py
@@ -32,7 +32,53 @@ def import_clubs(filepath):
     db.session.commit()
 
 
+def update_ecommerce(filepath):
+    with open(filepath) as file:
+        club_data = json.load(file)
+    for row in club_data:
+        club = Club.query.get(int(row['SKU']))
+        if club:
+            if not club.ecommerce:
+                club.ecommerce = row['Storefront Link']
+    db.session.commit()
+
+
+def update_clubs(filepath):
+    with open('etc/config.json') as file:
+        config = json.load(file)
+    password = config.get('USC_CLUB_MATCHER_PASSWORD')
+    with open(filepath) as file:
+        club_data = json.load(file)
+    for row in club_data:
+        club = Club.query.get(int(row['SKU']))
+        tag = Tag.query.filter_by(name=row['Tags']).first()
+        if not tag:
+            tag = Tag(name=row['Tags'])
+            db.session.add(tag)
+        if club:
+            club.name = row['Name']
+            club.email = row['Email']
+            club.description = row['Short description']
+            club.ecommerce = row['Storefront Link']
+            club.western_link = row['WL Address']
+            club.tag = tag
+        else:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_club = Club(
+                id=row['SKU'],
+                name=row['Name'],
+                email=row['Email'],
+                password=hashed_password,
+                description=row['Short description'],
+                ecommerce=row['Storefront Link'],
+                western_link=row['WL Address'],
+                tag=tag
+            )
+            db.session.add(new_club)
+    db.session.commit()
+
+
 app = create_app()
 if __name__ == '__main__':
     with app.app_context():
-        import_clubs('data/clubs.json')
+        update_clubs('data/clubs_updated.json')
