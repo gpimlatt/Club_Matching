ef05a3ab87dd98c7ac1f31999a3ff8966ba36c1a
Nikolas North <nikolasn09@gmail.com>
2020-10-22 11:42:08 -0400

    Modify import script

--- import.py
+++ import.py
@@ -1,69 +1,53 @@
-import os
+"""Script for creating/updating club accounts from a JSON file.
+
+When executing this script you must provide a name of a JSON file which
+contains the club information. This file must be stored inside of
+`Club_Matcher/data/` prior to executing the command. Otherwise, a FileNotFound
+error will occur.
+
+How to execute this script:
+`python import.py filename.json`
+
+(Where filename.json is `Club_Matcher/data/filename.json` and stores the club
+data.)
+"""
+
+import sys
 import json
-import re
 from clubmatcher import create_app, db, bcrypt
-from clubmatcher.main.models import Club, Tag, Statistic
+from clubmatcher.main.models import Club, Tag
 
 
-def import_clubs(filepath):
-    with open('etc/config.json') as file:
-        config = json.load(file)
-    password = config.get('USC_CLUB_MATCHER_PASSWORD')
-    with open(filepath) as file:
-        clubs = json.load(file)
-    for club in clubs:
-        if club['Email']:
-            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            tag = Tag.query.filter_by(name=club['Tags']).first()
-            if not tag:
-                tag = Tag(name=club['Tags'])
-                db.session.add(tag)
-            new_club = Club(
-                id=club['SKU'],
-                name=club['Name'],
-                email=club['Email'],
-                password=hashed_password,
-                description=club['Short description'],
-                ecommerce=club['Storefront Link'],
-                western_link=club['WL Address'],
-                tag=tag
-            )
-            db.session.add(new_club)
-    db.session.commit()
+def add_club(clubs):
+    """Add new clubs to the database or update existing clubs.
 
+    Args:
+        clubs:
 
-def update_ecommerce(filepath):
-    with open(filepath) as file:
-        club_data = json.load(file)
-    for row in club_data:
-        club = Club.query.get(int(row['SKU']))
-        if club:
-            if not club.ecommerce:
-                club.ecommerce = row['Storefront Link']
-    db.session.commit()
+    Returns:
+        True if clubs have been successfully created/updated.
+        False otherwise.
 
-
-def update_clubs(filepath):
-    with open('etc/config.json') as file:
-        config = json.load(file)
-    password = config.get('USC_CLUB_MATCHER_PASSWORD')
-    with open(filepath) as file:
-        club_data = json.load(file)
-    for row in club_data:
-        club = Club.query.get(int(row['SKU']))
+    Raises:
+        ValueError: An error occured querying the database for
+    """
+    for club in club_data:
+        existing_club = Club.query.get(int(row['SKU']))
         tag = Tag.query.filter_by(name=row['Tags']).first()
-        if not tag:
+        if not existing_tag:
             tag = Tag(name=row['Tags'])
             db.session.add(tag)
-        if club:
-            club.name = row['Name']
-            club.email = row['Email']
-            club.description = row['Short description']
-            club.ecommerce = row['Storefront Link']
-            club.western_link = row['WL Address']
-            club.tag = tag
+        if existing_club:
+            existing_club.name = row['Name']
+            existing_club.email = row['Email']
+            existing_club.description = row['Short description']
+            existing_club.ecommerce = row['Storefront Link']
+            existing_club.western_link = row['WL Address']
+            existing_club.tag = tag
         else:
-            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            hashed_password = bcrypt.generate_password_hash(
+                password
+            ).decode('utf-8')
             new_club = Club(
                 id=row['SKU'],
                 name=row['Name'],
@@ -76,14 +60,38 @@ def update_clubs(filepath):
             )
             db.session.add(new_club)
     db.session.commit()
+    return True
 
 
-app = create_app()
+def main():
+    """Opens file passed in as arg and converts data to dictionary of clubs.
+
+    Returns:
+        None
+    """
+    try:
+        with open("data/" + sys.argv[1]) as file:
+            clubs = json.load(file)
+        is_uploaded = add_club(clubs)
+        if is_uploaded:
+            print("Clubs uploaded successfully.")
+    except IndexError as indexerror:
+        print(
+            f"""
+    IndexError: {indexerror}
+
+    This script requires a JSON file holding club data.
+    (JSON file needs to be stored inside `Club_Matcher/data`)
+        `python import.py filename.json`
+
+    (The above command will upload events stored in `Club_Matcher/data/filename.json)
+"""
+        )
+    except FileNotFoundError as filenotfound:
+        print("Error: File not found.")
+
+
 if __name__ == '__main__':
+    app = create_app()
     with app.app_context():
-        statistic = Statistic.query.get(1)
-        if not statistic:
-            statistic = Statistic()
-            db.session.add(statistic)
-            db.session.commit()
-        update_clubs('data/new_clubs.json')
+        main()
